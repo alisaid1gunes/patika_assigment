032877e91541ce6d9780d1a9a6688d3d3b39bbcc
alisaidgunes <alisaidgunes1@gmail.com>
2021-10-21 18:00:18 +0300

    Create: CourseEnrollment Model

---
+++ models/CourseEnrollment.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Schema.Types;
+
+const completedLessonsSchema = new mongoose.Schema({
+  lessonId: {
+    type: ObjectId,
+    ref: 'Lesson',
+    require: true,
+  },
+});
+
+const CourseEnrollmentSchema = new mongoose.Schema({
+  userId: {
+    type: ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  courseId: {
+    type: ObjectId,
+    ref: 'Course',
+    require: true,
+  },
+  lastVisitedLesson: {
+    type: ObjectId,
+    ref: 'Lesson',
+    require: true,
+  },
+  lastCompletedLesson: {
+    type: ObjectId,
+    ref: 'Lesson',
+    require: true,
+  },
+  completedLessons: [completedLessonsSchema],
+});
+
+CourseEnrollmentSchema.set('timestamps', true);
+
+module.exports = mongoose.model('CourseEnrollment', CourseEnrollmentSchema);
