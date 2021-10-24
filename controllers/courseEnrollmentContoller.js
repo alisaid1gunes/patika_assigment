e72eb4ddaedccefc86af63011310d0ac37898821
alisaidgunes <alisaidgunes1@gmail.com>
2021-10-24 21:40:01 +0300

    Cretae: courseEnrollmentController

---
+++ controllers/courseEnrollmentContoller.js
@@ -0,0 +1,60 @@
+const CourseEnrollment = require('../models/CourseEnrollment');
+
+const CourseEnrollmentService = require('../services/CourseEnrollmentService');
+
+const CourseEnrollmentServiceInstance = new CourseEnrollmentService();
+
+const get = async (req, res) => {
+  try {
+    const result = await CourseEnrollmentServiceInstance.GetId(req.params.id);
+    res.json(result);
+  } catch (err) {
+    res.status(404).json('Kayıt bulunamadı. Hata:' + err);
+  }
+};
+
+const getAll = async (req, res) => {
+  try {
+    const result = await CourseEnrollmentServiceInstance.GetAll();
+    res.json(result);
+  } catch (err) {
+    res.status(404).json('Kayıtlar bulunamadı. Hata:' + err);
+  }
+};
+
+const save = async (req, res) => {
+  try {
+    const result = await CourseEnrollmentServiceInstance.Save(req.body);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json('Kayıt edilemedi. Hata:' + err);
+  }
+};
+
+const update = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await CourseEnrollmentServiceInstance.Update(req.body, id);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json('Güncellenemedi. Hata:' + err);
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await CourseEnrollmentServiceInstance.Delete(id);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json('Silinemedi. Hata:' + err);
+  }
+};
+
+module.exports = {
+  get,
+  getAll,
+  save,
+  update,
+  remove,
+};
